a25bcf901b828665a9662928005229d4118767f7
simondv4 <simondv4@gmail.com>
2024-07-02 13:52:51 +0300

    next homework

--- main.py
+++ main.py
@@ -10,8 +10,25 @@ class House:
         for i in range(1, new_floor + 1):
             print(i)
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
