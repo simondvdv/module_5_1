003da4b0320f9821c6b3a40018cba89c03ef932e
simondvdv <simondv4@gmail.com>
2024-07-07 21:07:27 +0300

    Module_5_3

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+        # House.houses_history.append(name)
+
+    def __del__(self):
+        print(f'{self.name}, снесен, но он останется в истории')
+        House.houses_history.remove(self.name)
 
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -48,25 +59,14 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(House.houses_history)
